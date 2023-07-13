59dbb497f1254440696b2b8fcdab9bd75bbc9eb9
Kishan <kishanvenkatesan@gmail.com>
2023-07-14 01:31:15 +0530

    added changing of bowlers

--- match.py
+++ match.py
@@ -12,13 +12,12 @@ class Match:
         self.field = field
         self.umpire = Umpire()
         self.commentator = Commentator()
+        self.previous_bowler = None
 
     def start_match(self):
         # Starting the match
         self.toss()
         self.select_batting_order()
-        self.select_bowlers()
-        # self.commentator.provide_commentary(None, None, None)
 
     def toss(self):
         # Simulate a toss to decide which team bats first
@@ -30,47 +29,42 @@ class Match:
         # Randomly set the batting order for the teams
         random.shuffle(self.team1.players)
         random.shuffle(self.team2.players)
-        self.team1.set_batting_order(self.team1.players)
-        self.team2.set_batting_order(self.team2.players)
+        self.team1.set_batting_order(self.team1.players.copy())
+        self.team2.set_batting_order(self.team2.players.copy())
 
-    def select_bowlers(self):
-        # Choose bowlers for the first over from the bowling team
-        self.team1.choose_bowler()
-        self.team2.choose_bowler()
+    def play(self):
+        self.start_match()
+        print("The match is starting....!")
+        print("\nThe ", self.current_batting_team.name, " has won the toss and will be batting first")
+        while not self.is_match_over():
+            batting_team = self.current_batting_team
+            bowling_team = self.current_bowling_team
 
-    def simulate_ball(self, batsman, bowler):
-        # Simulate a ball and update match stats
-        outcome = self.umpire.predict_outcome(batsman, bowler)
-        # runs_scored = random.choice([0, 1, 2, 3, 4, 6])
-        # Update match statistics based on the outcome
-        if outcome == 'wicket':
-            self.umpire.update_wickets()
-            batting_team = self.get_current_batting_team()
-            bowling_team = self.get_current_bowling_team()
-            if self.is_team_innings_completed(batting_team):
-                if self.is_team_innings_completed(bowling_team):
-                    exit()
-                else:
-                    self.change_innings()
-            else:
-                batting_team.batting_order.pop(0)
-                # Remove batsman from the field
-        elif outcome == 'boundary':
-            self.umpire.update_score(4)
-            batting_team = self.get_current_batting_team()
-            batting_team.add_score(4)  # Add 4 to the batting team's score
-        # else
+            self.simulate_over(batting_team, bowling_team)
 
-        # Generate commentary based on the match stats and ongoing game events
-        commentary = self.commentator.provide_commentary(batsman, bowler, outcome)
-        print(commentary)
+            if self.is_team_innings_completed(self.current_batting_team):
+                # if self.is_team_innings_completed(self.current_bowling_team):
+                #     return
+                # else:
+                self.change_innings()
 
     def simulate_over(self, batting_team, bowling_team):
-        # Simulate a complete over
         if not batting_team.batting_order:
             return
 
-        bowler = bowling_team.current_bowler
+        bowling_team = self.team1 if self.current_bowling_team == self.team1 else self.team2
+        batting_team = self.current_batting_team
+        while True:
+            bowler = bowling_team.choose_bowler(bowling_team.players, self.previous_bowler)
+            if bowler is None:
+                # bowler = bowling_team.choose_random(bowling_team.players)
+                print(len(batting_team.players))
+                bowler = random.choice(bowling_team.players)
+
+            batsman = batting_team.batting_order[0]
+            if bowler != batsman:  # and not (batsman == bowler):
+                break
+        # bowler = bowling_team.current_bowler
         print("\nChange of over. Bowler " + bowler.name + " is bowling")
         for _ in range(6):
             if not batting_team.batting_order:
@@ -79,26 +73,45 @@ class Match:
             batsman = batting_team.batting_order[0]
             self.simulate_ball(batsman, bowler)
 
+        # Update the previous bowler
+        self.previous_bowler = bowler
+
         # Update overs count for the umpire
         self.umpire.update_overs()
 
-    def play(self):
-        self.start_match()
-        print("The match is starting....!")
-        print("\nThe ", self.current_batting_team.name, " has won the toss and will be batting first")
-        while not self.is_match_over():
-            batting_team = self.get_current_batting_team()
-            bowling_team = self.get_current_bowling_team()
-
-            self.simulate_over(batting_team, bowling_team)
-
-            if self.is_team_innings_completed(self.current_batting_team):
+    def simulate_ball(self, batsman, bowler):
+        # Simulate a ball and update match stats
+        outcome = self.umpire.predict_outcome(batsman, bowler)
+        # runs_scored = random.choice([0, 1, 2, 3, 4, 6])
+        # Update match statistics based on the outcome
+        if outcome == 'wicket':
+            self.umpire.update_wickets()
+            batting_team = self.current_batting_team
+            if self.is_team_innings_completed(batting_team):
+                # if self.is_team_innings_completed(bowling_team):
+                #     exit()
+                # else:
                 self.change_innings()
+            else:
+                batting_team.batting_order.pop(0)
+                # Remove batsman from the field
+        elif outcome == 'boundary':
+            self.umpire.update_score(4)
+            batting_team = self.current_batting_team
+            batting_team.add_score(4)  # Add 4 to the batting team's score
+        # else
+
+        # Generate commentary based on the match stats and ongoing game events
+        commentary = self.commentator.provide_commentary(batsman, bowler, outcome)
+        print(commentary)
 
     def change_innings(self):
         # Switch batting and bowling teams for the second innings
         print("\nThe innings has been completed and ", self.current_bowling_team.name, " will be batting now")
+        self.current_batting_team.is_batted = True
+        self.previous_bowler = None
         self.current_batting_team, self.current_bowling_team = self.current_bowling_team, self.current_batting_team
+
         # self.commentator.provide_commentary(None, None, None)
 
     def is_match_over(self):
@@ -107,22 +120,8 @@ class Match:
 
     def is_team_innings_completed(self, team):
         # Check if the innings is completed for a specific team (e.g., all overs completed or all players out)
+        # return team.is_batted == True or team.batting_order == [] or self.umpire.get_overs() >= 20
         return team.batting_order == [] or self.umpire.get_overs() >= 20
-
-    def get_current_batting_team(self):
-        # Determine the current batting team based on the match conditions
-        if self.current_batting_team == self.team1:
-            return self.team1
-        else:
-            return self.team2
-
-    def get_current_bowling_team(self):
-        # Determine the current bowling team based on the match conditions
-        return self.current_bowling_team
-
-    # def is_innings_completed(self):
-    # Check if the current innings is completed (e.g., all overs completed)
-    #   return True  # Modify the condition based on your specific requirements
 
     def display_match_summary(self):
         # Display match summary based on the match statistics
--- team.py
+++ team.py
@@ -9,6 +9,7 @@ class Team:
         self.batting_order = []
         self.current_bowler = None
         self.score = 0
+        self.is_batted = False
 
     def select_captain(self, player):
         self.captain = player
@@ -16,8 +17,34 @@ class Team:
     def set_batting_order(self, order):
         self.batting_order = order
 
-    def choose_bowler(self):
-        self.current_bowler = random.choice(self.players)
+    def choose_bowler(self, players, previous_bowler):
+        # self.current_bowler = random.choice(self.players)
+        eligible_bowlers = [player for player in players if player.bowling_skill > 0 and player != previous_bowler]
+        if not eligible_bowlers:
+            return None
+        else:
+            # Calculate the weight for each bowler based on their skill and experience
+            weights = [bowler.bowling_skill * bowler.experience for bowler in eligible_bowlers]
+            total_weight = sum(weights)
+            # Generate a random number within the total weight range
+            random_num = random.uniform(0, total_weight)
+            # Choose the bowler based on the weighted random selection
+            cumulative_weight = 0
+            for i, bowler in enumerate(eligible_bowlers):
+                cumulative_weight += weights[i]
+                if random_num <= cumulative_weight:
+                    if bowler is not None:
+                        self.current_bowler = bowler
+                        return bowler
+        bowler = random.choice(players)
+        return bowler
+
+    def choose_random(self, players):
+        if len(players) > 1:
+            return random.choice(self.players)
+        else:
+            self.current_bowler = self.players[0]
+            return self.current_bowler
 
     def add_score(self, runs):
         self.score += runs
